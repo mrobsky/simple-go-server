3feaf61da0b3dcd52a7ff903f849e36a053ef494
Mitch Mikulsky <mitchmikulsky@gmail.com>
2022-01-28 16:35:43 -0500

    Run chi server

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func startServer(port string) {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root."))
+	})
+
+	fmt.Printf("Server listening on port: %v", port)
+	http.ListenAndServe(":"+port, r)
+
+}
+
+func main() {
+
+	startServer("8080")
+}
